8feaadb15c0a24f80d31e5e98f0e6f1581506777
Ahmad Nourallah <ahmadnurallah@gmail.com>
2024-06-10 05:54:03 -0400

    Create the logic to play a round

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ function getHumanChoice() {
     }
     return getHumanChoice();
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock") { console.log("It's a tie!"); }
+        else if (computerChoice === "paper") { console.log("You lose! Paper beats rock"); ++computerScore; }       
+        else if (computerChoice === "scissors") { console.log("You win! Rock beats scissors"); ++humanScore; }
+
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "paper") { console.log("It's a tie!"); }
+        else if (computerChoice === "scissors") { console.log("You lose! Scissors beat paper"); ++computerScore; }       
+        else if (computerChoice === "rock") { console.log("You win! Paper beats rock"); ++humanScore; }
+
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "scissors") { console.log("It's a tie!"); }
+        else if (computerChoice === "rock") { console.log("You lose! Rock beats scissors"); ++computerScore; }       
+        else if (computerChoice === "paper") { console.log("You win! Scissors beat paper"); ++humanScore; }
+    
+
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
